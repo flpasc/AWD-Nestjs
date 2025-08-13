0954f0e9b8787917fb5916b01c4aa16ad27cc16d
Florian <florianpaulschoene@gmail.com>
2025-08-13 12:53:43 +0200

    remove console.log, remove unnecessary <ul> inside volume.book.map, add Promise for PageProps

--- src/app/volumes/[slug]/page.tsx
+++ src/app/volumes/[slug]/page.tsx
@@ -1,26 +1,25 @@
 import { volumes } from "@/lib/data";
 import Image from "next/image";
+import { notFound } from "next/navigation";
 
-type PageProps = { params: { slug: string } };
+type PageProps = { params: Promise<{ slug: string }> };
 
 export default async function VolumePage({ params }: PageProps) {
   const { slug } = await params;
   const volume = volumes.find((volume) => volume.slug === slug);
-  if (!volume) return;
 
-  console.log(volume.cover);
+  if (!volume) return notFound();
+
   return (
     <main>
       <h1>{volume.title}</h1>
       <p>{volume.description}</p>
       <ul>
         {volume.books.map((book) => (
-          <ul key={book.ordinal}>
-            <li>
-              <h2>{book.title}</h2>
-              <p>{book.ordinal}</p>
-            </li>
-          </ul>
+          <li key={book.ordinal}>
+            <h2>{book.title}</h2>
+            <p>{book.ordinal}</p>
+          </li>
         ))}
       </ul>
       <Image alt={volume.title} src={volume.cover} width={200} height={200} />
